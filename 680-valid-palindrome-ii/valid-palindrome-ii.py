9ebf15946c6d8d3495a7bf0f3bcb85386c02c8db
sk-jaheed <jaheedshaik40@gmail.com>
2026-05-18 09:21:52 +0530

    Time: 31 ms (88.29%) | Memory: 19.4 MB (96.57%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+            else:
+                left_skip = s[left+1:right+1]
+                right_skip = s[left:right]
+                return left_skip == left_skip[::-1] or right_skip == right_skip[::-1]
+        return True
+
+        
