77bf1248e9cf166316e8a22cab23857d59bfca68
sk-jaheed <jaheedshaik40@gmail.com>
2026-07-10 17:32:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (42.66%) - LeetSync

---
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
@@ -0,0 +1,29 @@
+class Solution:
+    def mergeArrays(self, nums1, nums2):
+        result = []
+        i = 0
+        j = 0
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i][0] == nums2[j][0]:
+                result.append([nums1[i][0], nums1[i][1] + nums2[j][1]])
+                i += 1
+                j += 1
+
+            elif nums1[i][0] < nums2[j][0]:
+                result.append(nums1[i])
+                i += 1
+
+            else:
+                result.append(nums2[j])
+                j += 1
+
+        while i < len(nums1):
+            result.append(nums1[i])
+            i += 1
+
+        while j < len(nums2):
+            result.append(nums2[j])
+            j += 1
+
+        return result
