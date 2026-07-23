227b2ec5908433f20cff542c2b0102a3f29cd07f
sk-jaheed <jaheedshaik40@gmail.com>
2026-07-23 18:32:18 +0530

    Time: 16 ms (65.88%) | Memory: 19.6 MB (32.94%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,3 +1,8 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        return not (Counter(ransomNote)-Counter(magazine))
+        freq = Counter(magazine)
+        for char in ransomNote:
+            if char not in freq or freq[char] == 0:
+                return False
+            freq[char] -= 1
+        return True
