521919d244dbcdfa6e3fa408aec819a062557721
sk-jaheed <jaheedshaik40@gmail.com>
2026-07-24 10:45:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.24%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,8 +1,23 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        freq = Counter(stones)
         count = 0
-        for stone in stones:
-            if stone in jewels:
-                count += 1
+        for char in jewels:
+            if char in freq:
+                count += freq[char]
         return count
+        
+
+
+
+
+
+
+
+#count = 0
+       # for stone in stones:
+          #  if stone in jewels:
+              #  count += 1
+      #  return count
+
         
