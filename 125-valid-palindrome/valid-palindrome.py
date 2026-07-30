82a21d1319852ff1784e40e00bffa75eae3ea885
sk-jaheed <jaheedshaik40@gmail.com>
2026-07-30 21:05:36 +0530

    Time: 13 ms (10.84%) | Memory: 19.7 MB (52.08%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,5 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        cleaned = [char.lower() for char in s if char.isalnum()]
-        return cleaned == cleaned[::-1]
+        cleaned_string = ""
+        for char in s:
+            if char.isalnum():
+                cleaned_string += char.lower()
+        reversed_string = ""
+        for i in range(len(cleaned_string)-1,-1,-1):
+            reversed_string += cleaned_string[i]
+        if cleaned_string == reversed_string:
+            return True
+        else:
+            return False
         
